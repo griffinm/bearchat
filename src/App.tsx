438c451f27b1dce3ade4c151dadc1eab9b6dc086
Griffin <griffin@yardzen.com>
2024-06-30 14:29:35 -0400

    Adds MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
     <div className="">
-      This is the app
+      <Typography variant="h1" component="h1" align="center">
+        Hello World
+      </Typography>
     </div>
   );
 }
