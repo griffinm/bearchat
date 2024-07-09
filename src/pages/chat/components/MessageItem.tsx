48475fb039cb4df7607507caa3001ff17c54c7ce
griffinm <griffin.mahoney@gmail.com>
2024-07-09 19:28:24 -0400

    Adds times

--- src/pages/chat/components/MessageItem.tsx
+++ src/pages/chat/components/MessageItem.tsx
@@ -1,13 +1,23 @@
 import { Message, User } from "../../../utils/types";
 import classNames from "classnames";
+import { format } from "date-fns";
+import { useState } from "react";
 
 interface Props {
   message: Message;
   isSender: boolean;
   user: User;
+  showTimestamp: boolean;
 }
 
-export function MessageItem({ message, isSender }: Props) {
+export function MessageItem({
+  message,
+  isSender,
+  showTimestamp,
+  user,
+}: Props) {
+  const [showTime, setShowTime] = useState(showTimestamp);
+
   const containerClasses = classNames("flex items-center", {
     "justify-end": isSender,
     "justify-start": !isSender,
@@ -18,10 +28,31 @@ export function MessageItem({ message, isSender }: Props) {
     "bg-gray-200": !isSender,
   });
 
+  const timeClasses = classNames("mb-4 text-xs text-gray-500", {
+    "text-right": isSender,
+    "text-left": !isSender,
+  });
+
+  const toggleShowTime = () => {
+    if (showTimestamp) {
+     // The parent told this to show the time
+      return
+    }
+
+    setShowTime(!showTime);
+  }
+
   return (
     <div className={containerClasses}>
-      <div className={bubbleClasses}>
-        {message.content}
+      <div className="flex flex-col">
+        <div className={bubbleClasses} onClick={toggleShowTime}>
+          {message.content}
+        </div>
+        {showTime && (
+          <div className={timeClasses}>
+            {format(new Date(message.createdAt), "h:mm aaa")}
+          </div>
+        )}
       </div>
     </div>
   )
--- src/pages/chat/components/Messages.tsx
+++ src/pages/chat/components/Messages.tsx
@@ -19,17 +19,32 @@ export function Messages({
     })
   }, [messages])
 
+  const renderMessage = (message: Message) => {
+    let isLastOfSender = false
+    if (sortedMessages.length > sortedMessages.indexOf(message)) {
+      const nextMessage = sortedMessages[sortedMessages.indexOf(message) + 1]
+      if (nextMessage) {
+        isLastOfSender = nextMessage.userId !== message.userId
+      } else {
+        isLastOfSender = true
+      }
+    }
+
+    return (
+      <MessageItem
+        showTimestamp={isLastOfSender}
+        key={message.id}
+        message={message}
+        isSender={message.userId === currentUser.id}
+        user={participants.find((user) => user.id === message.userId)!}
+      />
+    )
+  }
+
   return (
     <div className="grow flex justify-center">
       <div className="grow p-5 max-w-[700px]">
-        {sortedMessages.map((message) => (
-          <MessageItem 
-            key={message.id}
-            message={message}
-            isSender={message.userId === currentUser.id}
-            user={participants.find((user) => user.id === message.userId)!}
-          />
-        ))}
+        {sortedMessages.map((message) => renderMessage(message) )}
       </div>
     </div>
   )
