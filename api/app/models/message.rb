22b09f996c2a0715f144574303a462c46c64b612
griffinm <griffin.mahoney@gmail.com>
2024-07-09 19:06:22 -0400

    Kind of getting sockets working

--- api/app/models/message.rb
+++ api/app/models/message.rb
@@ -3,4 +3,21 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
+
+  after_create :broadcast_message
+  
+  # TODO: make this async
+  private def broadcast_message
+    ActionCable.server.broadcast("ChatChannel", self.to_json)
+  end
+
+  def to_json
+    {
+      id: self.id,
+      content: self.content,
+      userId: self.user_id,
+      conversationId: self.conversation_id,
+      createdAt: self.created_at,
+    }
+  end
 end
