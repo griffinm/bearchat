03ed28b733e21d01c23c6244440b6a55a8c12df6
griffinm <griffin.mahoney@gmail.com>
2024-07-10 18:38:34 -0400

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,12 @@ NODE_ENV=production npm run build
 
 cd ./build/
 
-ssh ubuntu@44.202.222.180 -t 'bash -l -c "rm -rf /var/www/bearchat-ui/*"'
-scp -r ./* ubuntu@44.202.222.180:/var/www/bearchat-ui/
+ssh ubuntu@44.192.59.78 -t 'bash -l -c "rm -rf /var/www/bearchat-ui/*"'
+scp -r ./* ubuntu@44.192.59.78:/var/www/bearchat-ui/
 
-ssh ubuntu@44.202.222.180 -t 'bash -l -c "sudo systemctl restart nginx"'
+ssh ubuntu@44.192.59.78 -t 'bash -l -c "sudo systemctl restart nginx"'
+
+ssh ubuntu@44.192.59.78 -t 'bash -l -c "cd /var/www/bearchat && git pull origin release"'
+ssh ubuntu@44.192.59.78 -t 'bash -l -c "sudo systemctl restart bearchat-api"'
+
+git checkout master
