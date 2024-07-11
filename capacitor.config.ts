9407f37d805f3c36e3ed5311235e2104d91121ec
griffinm <griffin.mahoney@gmail.com>
2024-07-11 14:55:31 -0400

    Google stuff

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bearchat.app',
   appName: 'bearchat',
-  webDir: 'build'
+  webDir: 'build',
+  
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  },
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,42 @@ import '@fontsource/roboto/700.css';
 import { UserProvider } from './providers/UserProvider';
 import { Router } from './components/router/Router';
 import { WSProvider } from './providers/wsProvider';
+import {
+  FirebaseMessaging,
+  GetTokenOptions,
+} from "@capacitor-firebase/messaging";
+import { PushNotificationSchema, PushNotifications, Token, ActionPerformed } from '@capacitor/push-notifications';
+import { useEffect } from 'react';
 
 function App() {
+  // Push notifications
+  useEffect(() => {
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    PushNotifications.addListener('registration', (token: Token) => {
+      console.log('Push registration success, token: ' + token.value);
+    });
+
+    PushNotifications.addListener('registrationError', (error: any) => {
+      console.log('Error on registration: ' + JSON.stringify(error));
+    });
+
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+      console.log('Push received: ' + JSON.stringify(notification));
+    });
+
+    PushNotifications.addListener('pushNotificationActionPerformed', (notification: ActionPerformed) => {
+      console.log('Push action performed: ' + JSON.stringify(notification));
+    });
+  }, [])
+
   return (
     <UserProvider>
       <WSProvider>
